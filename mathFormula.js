1cd85d626e45152697957b2b380a3576f63844d2
Kreisler57 <70900694+Kreisler57@users.noreply.github.com>
2021-06-26 13:32:07 +0700

    Add files via upload

---
+++ mathFormula.js
@@ -0,0 +1,29 @@
+//Q1:
+
+function cariVolumeKubus(panjang,lebar,tinggi){
+    return ( panjang * lebar * tinggi);
+};
+
+//Q2:
+
+function cariKelilingSegitiga(sisi1,sisi2,sisi3){
+    return (sisi1 + sisi2 + sisi3);
+};
+
+//Q3:
+
+function cariLuasSegitiga (alas, tinggi){
+    return (1/2 * alas * tinggi);
+};
+
+//Q4:
+
+function cariVolumePrisma (panjang, lebar, tinggi){
+    return (((panjang * lebar) / 2) * tinggi);
+};
+
+//Q5:
+
+function cariCustom (s1, s2, s3, s4, s5, s6, s7, s8, s9){
+    return (s1 * s2 * (s3 * s4 * ( s5 * s6 * ( s7 * s8 * s9))));
+};
